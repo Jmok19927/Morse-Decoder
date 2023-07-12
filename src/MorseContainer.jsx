cf7cc6bbb7744a0fd1f8946d656fc39099af2719
Jmok19927 <jmok19927@gmail.com>
2023-07-12 15:03:49 -0700

    changing to functional component

--- src/MorseContainer.jsx
+++ src/MorseContainer.jsx
@@ -1,51 +1,86 @@
 import React from 'react';
 import MorseArea from './MorseArea.jsx';
-
-class MorseContainer extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      input: ''
-    };
-  }
-
-  handleTyping(e) {
-    let newString = e.target.value;
-    this.setState({input: newString})
-  }
-
-  handleOnMouseDown() {
+import {useState} from 'react';
+function MorseContainer() {
+  function handleOnMouseDown() {
     console.log('handleOnMouseDown');
   }
 
-  handleOnMouseUp() {
+  function handleOnMouseUp() {
     console.log('handleOnMouseUp');
   }
 
-  handleOnTouchStart() {
+  function handleOnTouchStart() {
     console.log('handleOnTouchStart');
   }
 
-  handleOnTouchEnd() {
+  function handleOnTouchEnd() {
     console.log('handleOnTouchEnd');
   }
 
-  render() {
-    return (
-      <div>
-        Main Container Component
-        <form>
-          <input className='form' placeholder='Your morse input here.' type='text' onChange={this.handleTyping.bind(this)} value={this.state.input}/>
-        </form>
-        <MorseArea
-        handleOnMouseDown={this.handleOnMouseDown.bind(this)}
-        handleOnMouseUp={this.handleOnMouseUp.bind(this)}
-        handleOnTouchStart={this.handleOnTouchStart.bind(this)}
-        handleOnTouchEnd={this.handleOnTouchEnd.bind(this)}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+    Main Container Component
+    <form>
+      <input className='form' placeholder='Your morse input here.' type='text' onChange={()=>{}} value={''}/>
+    </form>
+    <MorseArea
+    handleOnMouseDown={handleOnMouseDown}
+    handleOnMouseUp={handleOnMouseUp}
+    handleOnTouchStart={handleOnTouchStart}
+    handleOnTouchEnd={handleOnTouchEnd}
+    />
+  </div>
+  )
 }
+
+// class MorseContainer extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       input: ''
+//     };
+//   }
+
+  // handleTyping(e) {
+  //   let newString = e.target.value;
+  //   this.setState({input: newString})
+  // }
+
+  // handleOnMouseDown() {
+  //   console.log('handleOnMouseDown');
+  // }
+
+  // handleOnMouseUp() {
+  //   console.log('handleOnMouseUp');
+  // }
+
+  // handleOnTouchStart() {
+  //   console.log('handleOnTouchStart');
+  // }
+
+  // handleOnTouchEnd() {
+  //   console.log('handleOnTouchEnd');
+  // }
+
+
+
+//   render() {
+//     return (
+      // <div>
+      //   Main Container Component
+      //   <form>
+      //     <input className='form' placeholder='Your morse input here.' type='text' onChange={this.handleTyping.bind(this)} value={this.state.input}/>
+      //   </form>
+      //   <MorseArea
+      //   handleOnMouseDown={this.handleOnMouseDown.bind(this)}
+      //   handleOnMouseUp={this.handleOnMouseUp.bind(this)}
+      //   handleOnTouchStart={this.handleOnTouchStart.bind(this)}
+      //   handleOnTouchEnd={this.handleOnTouchEnd.bind(this)}
+      //   />
+      // </div>
+//     )
+//   }
+// }
 
 export default MorseContainer;
