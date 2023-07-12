ed885fa38caf176426b24e928a7fbcd13ae03bd7
Jmok19927 <jmok19927@gmail.com>
2023-07-12 15:20:59 -0700

    telegraph key basic functionality working

--- src/MorseContainer.jsx
+++ src/MorseContainer.jsx
@@ -1,28 +1,57 @@
 import React from 'react';
 import MorseArea from './MorseArea.jsx';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 function MorseContainer() {
+const [morseText, setMorseText] = useState('');
+const [longPress, setLongPress]  = useState(false);
+const [delay, setDelay] = useState(300);
+const timerRef = useRef();
+
+  function handleTextChange(e) {
+    let newString = e.target.value;
+    setMorseText(newString)
+  }
+
+  function clickDuration() {
+    timerRef.current = setTimeout(() => {
+      setLongPress(true);
+      let newString = morseText + '-';
+      setMorseText(newString);
+    }, delay)
+  }
+
   function handleOnMouseDown() {
     console.log('handleOnMouseDown');
+    clickDuration();
+    // let newString = morseText + '.';
+    // setMorseText(newString);
   }
 
   function handleOnMouseUp() {
     console.log('handleOnMouseUp');
+    clearTimeout(timerRef.current);
+    if (!longPress) {
+      let newString = morseText + '.';
+      setMorseText(newString);
+    }
+    setLongPress(false)
   }
 
   function handleOnTouchStart() {
     console.log('handleOnTouchStart');
+    clickDuration();
   }
 
   function handleOnTouchEnd() {
     console.log('handleOnTouchEnd');
+    clearTimeout(timerRef.current);
   }
 
   return (
     <div>
     Main Container Component
     <form>
-      <input className='form' placeholder='Your morse input here.' type='text' onChange={()=>{}} value={''}/>
+      <input className='form' placeholder='Your morse input here.' type='text' onChange={(e)=>{handleTextChange(e)}} value={morseText}/>
     </form>
     <MorseArea
     handleOnMouseDown={handleOnMouseDown}
